b7036817fe17b5bbd771765e8404c04090d91db1
Ryan <ryanmelopedro190@gmail.com>
2026-05-19 14:54:22 -0300

    adicionando input no algoritmo de verificação de vetores

--- algoritmo de verificação de vetores.py
+++ algoritmo de verificação de vetores.py
@@ -1,18 +1,29 @@
-vetor1 = [1,2,3,4,5,6]
-vetor2 = [2,4,6]
+vetor1 = []
+vetor2 = []
 
-vetoresIguais = True
+quantidade_de_valores_v1 = int(input("Insira a quantidade de valores do primeiro vetor: "))
+quantidade_de_valores_v2 = int(input("Insira a quantidade de valores do segundo vetor: "))
+
+for i in range(quantidade_de_valores_v1):
+    a = int(input("Insira os valores para o primeiro vetor:"))
+    vetor1.append(a)
+        
+for j in range(quantidade_de_valores_v2):
+    b = int(input("Insira os valores para o segundo vetor:"))
+    vetor2.append(b)
+
+vetores_iguais = True
 
 if len(vetor1) != len(vetor2):
-    vetoresIguais = False
+    vetores_iguais = False
 
 else:
     for i in vetor1:
         if i not in vetor2:
-            vetoresIguais = False
+            vetores_iguais = False
             break
 
-if vetoresIguais == True:
+if vetores_iguais == True:
     print("Vetores iguais")
 
 else:
